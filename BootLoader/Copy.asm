227e048adbb23df281c93212d26931ba4ac44bee
oliverskc <oskocek@rockefeller.edu>
2019-01-21 22:13:32 +0100

    added copy routine

--- BootLoader/Copy.asm
+++ BootLoader/Copy.asm
@@ -1,1 +1,16 @@
 ; Copies BX bytes from address DS:BX
+; Copy CX bytes from SI to DI
+Copy8:
+pusha
+Copy8_loop:
+cmp     CX
+jz      Copy8End
+mov     AX, [DS:SI]
+mov     [DS:DI], AX
+inc     DS
+inc     SI
+dec     CX
+jmp     Copy_loop
+Copy8End:
+popa
+ret
